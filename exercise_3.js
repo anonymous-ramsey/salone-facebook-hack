0c544d9ff2a2b6ae67e1e64bcd91046d352767d6
mohamedramsey <36452464+mohamed-ramsey@users.noreply.github.com>
2019-11-08 18:20:29 +0000

    Update exercise_3.js

--- exercise_3.js
+++ exercise_3.js
@@ -1,27 +1,8 @@
 var MyData = [
-    {
-        "key": "value",
-        "ab": "hi there",
-        "we": "say what"
-    }
-];
-function dynamicSort(property) {
-    var sortOrder = 1;
+    "key: value",
+    "ab: hi there",
+    "we: say what"
+]
 
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-
-    return function (a,b) {
-        if(sortOrder == -1){
-            return b[property].localeCompare(a[property]);
-        }else{
-            return a[property].localeCompare(b[property]);
-        }
-    }
-}
-
-MyData.sort(dynamicSort("key"));
-
+MyData.sort();
 console.log(MyData);
