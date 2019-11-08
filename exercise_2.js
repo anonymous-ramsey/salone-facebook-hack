38ccd140f15c947f12a3e4a5107a49e30a737fde
mohamedramsey <36452464+mohamed-ramsey@users.noreply.github.com>
2019-11-08 16:44:39 +0000

    Update exercise_2.js

--- exercise_2.js
+++ exercise_2.js
@@ -1,2 +1,15 @@
-const oddNumbers = [1, 2, 3, 4, 5, 7].filter((number) => number%2!==0);
-console.log(oddNumbers);
+
+let oddNumbers1 = [1, 2, 3, 4, 5, 7];
+const returnOdds = () => {
+  oddNumbers1.filter((numbers) => {
+    if(numbers%2 !== 0){
+      console.log(`${numbers}`)
+    }
+  })
+}
+returnOdds();
+
+//or u can simply filter and get all the odd numbers like this
+
+let allNumbers = [1, 2, 3, 4, 5, 7].filter((oddNumbers) => oddNumbers%2!==0);
+console.log(`the odd numbers are: ${allNumbers}`);
